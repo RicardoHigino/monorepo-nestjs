9cb3019511ff8a4a4402f617ab75049ddc6cf980
Ricardo Higino <ricard.code@gmail.com>
2023-12-01 19:12:39 -0300

    feat: docker build

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ "$SERVER_NAME" = "first-app" ] ; then
+if [ "$_APP_NAME" = "first-app" ] ; then
     echo "Running start"
     npm run start
-elif [ "$SERVER_NAME" = "second-app" ] ; then
+elif [ "$_APP_NAME" = "second-app" ] ; then
     echo "Running dev"
     npm run start:second-app
 else
